06b198af32fde078ce6acfd2a41911228d76502f
DianaG-M <aries_di@hotmail.com>
2019-07-05 16:06:56 -0500

    Separa los estilos del componente del nav y los agrega a la carpeta del componente

--- src/app/components/shared/nav/nav.component.ts
+++ src/app/components/shared/nav/nav.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styles: []
+  styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
 
